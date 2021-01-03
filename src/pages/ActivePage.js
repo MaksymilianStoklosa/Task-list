b6b16fe4e1fe9dd775e8853a42de002ce2210b1f
Maksymilian Stoklosa <stoklosa.maksymilian@gmail.com>
2021-01-03 20:21:02 +0100

    Add toggle button

--- src/pages/ActivePage.js
+++ src/pages/ActivePage.js
@@ -2,9 +2,10 @@ import React, { useState } from 'react';
 import MainTemplate from 'templates/MainTemplate';
 import Header from 'components/Header';
 import TaskForm from 'components/TaskForm';
+import ToggleButton from 'components/ToggleButton';
 
 const ActivePage = () => {
-  const [openForm, setOpenForm] = useState(true);
+  const [openForm, setOpenForm] = useState(false);
 
   const toggleForm = () => {
     setOpenForm((form) => !form);
@@ -17,6 +18,7 @@ const ActivePage = () => {
         description="You have (0) active tasks"
       />
       {openForm ? <TaskForm toggleForm={toggleForm} /> : null}
+      {!openForm ? <ToggleButton toggleForm={toggleForm} /> : null}
     </MainTemplate>
   );
 };
