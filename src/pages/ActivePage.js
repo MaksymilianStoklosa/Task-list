37c8e228e27a5c23e2404ada3f5420761feb14f7
Maksymilian Stoklosa <stoklosa.maksymilian@gmail.com>
2021-01-03 19:57:58 +0100

    Add task form to active page

--- src/pages/ActivePage.js
+++ src/pages/ActivePage.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MainTemplate from 'templates/MainTemplate';
 import Header from 'components/Header';
+import TaskForm from 'components/TaskForm';
 
-const ActivePage = () => (
-  <MainTemplate>
-    <Header
-      heading="What will you do today?"
-      description="You have (0) active tasks"
-    />
-  </MainTemplate>
-);
+const ActivePage = () => {
+  const [openForm, setOpenForm] = useState(true);
+
+  const toggleForm = () => {
+    setOpenForm((form) => !form);
+  };
+
+  return (
+    <MainTemplate>
+      <Header
+        heading="What will you do today?"
+        description="You have (0) active tasks"
+      />
+      {openForm ? <TaskForm toggleForm={toggleForm} /> : null}
+    </MainTemplate>
+  );
+};
 
 export default ActivePage;
