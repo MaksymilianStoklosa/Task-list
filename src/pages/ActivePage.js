3cb8f68d95d74aee247c51e2004cfee043053627
Maksymilian Stoklosa <stoklosa.maksymilian@gmail.com>
2021-01-03 12:56:30 +0100

    Initial pages

---
+++ src/pages/ActivePage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ActivePage = () => (
+  <h1>Active Page</h1>
+);
+
+export default ActivePage;
---
+++ src/pages/CompletedPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CompletedPage = () => (
+  <h1>Completed Page</h1>
+);
+
+export default CompletedPage;
