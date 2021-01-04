58836237ff7b4afa885bdd1a4809f814953e6aad
Maksymilian Stoklosa <stoklosa.maksymilian@gmail.com>
2021-01-04 07:36:36 +0100

    Change file name

--- src/pages/ActivePage.js
+++ src/pages/ActivePage.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import MainTemplate from 'templates/MainTemplate';
 import Header from 'components/Header';
 import TaskForm from 'components/TaskForm';
-import ToggleButton from 'components/ToggleButton';
+import OpenFormButton from 'components/OpenFormButton';
 
 const ActivePage = () => {
   const [openForm, setOpenForm] = useState(false);
@@ -18,7 +18,7 @@ const ActivePage = () => {
         description="You have (0) active tasks"
       />
       {openForm ? <TaskForm toggleForm={toggleForm} /> : null}
-      {!openForm ? <ToggleButton toggleForm={toggleForm} /> : null}
+      {!openForm ? <OpenFormButton toggleForm={toggleForm} /> : null}
     </MainTemplate>
   );
 };
