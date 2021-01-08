fd0f0acc7d52a5173f066e55faf4dc2edc2aef35
Maksymilian Stoklosa <stoklosa.maksymilian@gmail.com>
2021-01-08 18:14:21 +0100

    Reduce code
    Added variable with path and link content and used map to create
    NavLinks.

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,39 +1,44 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const Sidebar = () => (
-  <aside className="sidebar">
-    <h1 className="sidebar__heading">Task list</h1>
-    <nav className="sidebar__navigation">
-      <ul>
-        <li>
-          <NavLink
-            className="sidebar__link"
-            exact
-            to="/"
-          >
-            active
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            className="sidebar__link"
-            to="/completed"
-          >
-            completed
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            className="sidebar__link"
-            to="/search"
-          >
-            search
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
-  </aside>
-);
+const Sidebar = () => {
+  const paths = [
+    {
+      to: '/',
+      content: 'active',
+    },
+    {
+      to: '/completed',
+      content: 'completed',
+    },
+    {
+      to: '/search',
+      content: 'search',
+    },
+  ];
+
+  return (
+    <aside className="sidebar">
+      <h1 className="sidebar__heading">Task list</h1>
+      <nav className="sidebar__navigation">
+        <ul>
+          {paths.map((link) => (
+            <li
+              key={link.content}
+            >
+              <NavLink
+                className="sidebar__link"
+                to={link.to}
+                exact
+              >
+                {link.content}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </aside>
+  );
+};
 
 export default Sidebar;
