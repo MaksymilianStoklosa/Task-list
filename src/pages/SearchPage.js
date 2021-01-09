1d2bcecd4c2f43058f68e58d13b534b0a0e1bb73
Maksymilian Stoklosa <stoklosa.maksymilian@gmail.com>
2021-01-09 16:20:21 +0100

    Add search input

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,14 +1,25 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import MainTemplate from 'templates/MainTemplate';
 import Header from 'components/Header';
 import TaskList from 'components/TaskList';
 import Task from 'components/Task';
 import { AppContext } from 'context/AppContext';
+import Button from 'components/Button';
 
 const SearchPage = () => {
   const { tasks } = useContext(AppContext);
+  const [searchValue, setSearchValue] = useState('');
+  const [searchInput, setSearchInput] = useState(false);
 
-  const allTasks = tasks.map((task) => (
+  const filtredTasks = tasks.filter((task) => {
+    if (searchValue === '') {
+      return task;
+    }
+    if (task.text.toLowerCase().includes(searchValue.toLowerCase())) {
+      return task;
+    }
+    return null;
+  }).map((task) => (
     <Task
       key={task.id}
       id={task.id}
@@ -25,7 +36,20 @@ const SearchPage = () => {
         heading="What you looking for?"
         description={`You have total (${tasks.length}) tasks`}
       />
-      <TaskList tasks={allTasks} />
+      <input
+        className="searchpage__input"
+        style={searchInput ? { transform: 'translateY(22rem)' } : null}
+        type="text"
+        onChange={(e) => setSearchValue(e.target.value)}
+        placeholder="Search ..."
+      />
+      <TaskList tasks={filtredTasks} />
+      <Button
+        classname="searchpage__button"
+        click={() => setSearchInput((visible) => !visible)}
+      >
+        <i className="fas fa-search" />
+      </Button>
     </MainTemplate>
   );
 };
